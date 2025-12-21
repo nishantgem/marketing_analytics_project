49d82a82f9ab33484c788a979f3d24ef5b927a55
nishantgem <nishant.gem@gmail.com>
2025-12-22 00:52:08 +0100

    Fix nanosecond timestamp conversion in staging models

---
+++ marketing_project/models/staging/social/stg_meta_mx.sql
@@ -0,0 +1,35 @@
+{{ config(materialized='view') }}
+
+select
+    to_timestamp(date / 1000000000)::date as date,
+    'meta_mx' as platform,
+
+    media_channel_ch,
+    type,
+    platform_pl,
+    data_source,
+    funding_source_fs,
+    sub_brand_sb,
+    product_category_pr,
+    campaign_name,
+    campaign_name_cn,
+    mindset_md,
+    quarter,
+    month,
+    cw_iso,
+    year,
+    division_bs,
+    business_activity,
+    kpi_pk,
+
+    costs,
+    cost_usd,
+    impressions,
+    clicks,
+    total_conversions,
+    total_conversions_revenue,
+    total_conversion_revenue_usd,
+    mobile_app_installs,
+    objective_ob
+
+from {{ source('social_raw', 'meta_mx') }}
