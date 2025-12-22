8186ba331f0fa1b754a4eb2632d3b589d75c0ead
nishantgem <nishant.gem@gmail.com>
2025-12-22 15:54:02 +0100

    Updated prep model with date fix + macros

--- marketing_project/models/mart/fact_marketing_performance.sql
+++ marketing_project/models/mart/fact_marketing_performance.sql
@@ -1,11 +1,31 @@
 {{ config(materialized='table') }}
 
 select
+    -- Keys & core metadata
     date,
     campaign_id,
-    campaign_name,
+    campaign_name_cn,
+    platform,
+    funding_source_fs,
+    kpi_pk,
+    objective_ob,
+    quarter,
+    month,
+    cw_iso,
+    year,
+    division_bs,
 
-    -- Platform-level fields (kept for Tableau)
+    -- Optional high‑level attributes
+    media_channel_ch,
+    type,
+    platform_pl,
+    data_source,
+    sub_brand_sb,
+    product_category_pr,
+    mindset_md,
+    business_activity,
+
+    -- Platform-level metrics
     meta_mx_cost_usd,
     meta_non_mx_cost_usd,
     pinterest_cost_usd,
@@ -31,143 +51,51 @@ select
     pinterest_revenue_usd,
     reddit_revenue_usd,
 
-    --------------------------------------------------------------------
-    -- Aggregated metrics using the macro
-    --------------------------------------------------------------------
+    -- Aggregated metrics
+    {{ safe_sum(['meta_mx_cost_usd', 'meta_non_mx_cost_usd', 'pinterest_cost_usd', 'reddit_cost_usd']) }} as total_spend_usd,
 
-    {{ safe_sum([
-        'meta_mx_cost_usd',
-        'meta_non_mx_cost_usd',
-        'pinterest_cost_usd',
-        'reddit_cost_usd'
-    ]) }} as total_spend_usd,
+    {{ safe_sum(['meta_mx_impressions', 'meta_non_mx_impressions', 'pinterest_impressions', 'reddit_impressions']) }} as total_impressions,
 
-    {{ safe_sum([
-        'meta_mx_impressions',
-        'meta_non_mx_impressions',
-        'pinterest_impressions',
-        'reddit_impressions'
-    ]) }} as total_impressions,
+    {{ safe_sum(['meta_mx_clicks', 'meta_non_mx_clicks', 'pinterest_clicks', 'reddit_clicks']) }} as total_clicks,
 
-    {{ safe_sum([
-        'meta_mx_clicks',
-        'meta_non_mx_clicks',
-        'pinterest_clicks',
-        'reddit_clicks'
-    ]) }} as total_clicks,
+    {{ safe_sum(['meta_mx_conversions', 'meta_non_mx_conversions', 'pinterest_conversions', 'reddit_conversions']) }} as total_conversions,
 
-    {{ safe_sum([
-        'meta_mx_conversions',
-        'meta_non_mx_conversions',
-        'pinterest_conversions',
-        'reddit_conversions'
-    ]) }} as total_conversions,
-
-    {{ safe_sum([
-        'meta_mx_revenue_usd',
-        'meta_non_mx_revenue_usd',
-        'pinterest_revenue_usd',
-        'reddit_revenue_usd'
-    ]) }} as total_revenue_usd,
+    {{ safe_sum(['meta_mx_revenue_usd', 'meta_non_mx_revenue_usd', 'pinterest_revenue_usd', 'reddit_revenue_usd']) }} as total_revenue_usd,
 
     -- ROAS
-    case 
-        when {{ safe_sum([
-            'meta_mx_cost_usd',
-            'meta_non_mx_cost_usd',
-            'pinterest_cost_usd',
-            'reddit_cost_usd'
-        ]) }} = 0 then null
-        else (
-            {{ safe_sum([
-                'meta_mx_revenue_usd',
-                'meta_non_mx_revenue_usd',
-                'pinterest_revenue_usd',
-                'reddit_revenue_usd'
-            ]) }}
-        ) / (
-            {{ safe_sum([
-                'meta_mx_cost_usd',
-                'meta_non_mx_cost_usd',
-                'pinterest_cost_usd',
-                'reddit_cost_usd'
-            ]) }}
-        )
+    case
+        when {{ safe_sum(['meta_mx_cost_usd', 'meta_non_mx_cost_usd', 'pinterest_cost_usd', 'reddit_cost_usd']) }} = 0
+            then null
+        else
+            {{ safe_sum(['meta_mx_revenue_usd', 'meta_non_mx_revenue_usd', 'pinterest_revenue_usd', 'reddit_revenue_usd']) }}
+            / {{ safe_sum(['meta_mx_cost_usd', 'meta_non_mx_cost_usd', 'pinterest_cost_usd', 'reddit_cost_usd']) }}
     end as roas_usd,
 
     -- CTR
-    case 
-        when {{ safe_sum([
-            'meta_mx_impressions',
-            'meta_non_mx_impressions',
-            'pinterest_impressions',
-            'reddit_impressions'
-        ]) }} = 0 then null
-        else (
-            {{ safe_sum([
-                'meta_mx_clicks',
-                'meta_non_mx_clicks',
-                'pinterest_clicks',
-                'reddit_clicks'
-            ]) }} * 1.0
-        ) / (
-            {{ safe_sum([
-                'meta_mx_impressions',
-                'meta_non_mx_impressions',
-                'pinterest_impressions',
-                'reddit_impressions'
-            ]) }}
-        )
+    case
+        when {{ safe_sum(['meta_mx_impressions', 'meta_non_mx_impressions', 'pinterest_impressions', 'reddit_impressions']) }} = 0
+            then null
+        else
+            {{ safe_sum(['meta_mx_clicks', 'meta_non_mx_clicks', 'pinterest_clicks', 'reddit_clicks']) }} * 1.0
+            / {{ safe_sum(['meta_mx_impressions', 'meta_non_mx_impressions', 'pinterest_impressions', 'reddit_impressions']) }}
     end as ctr,
 
     -- CPC
-    case 
-        when {{ safe_sum([
-            'meta_mx_clicks',
-            'meta_non_mx_clicks',
-            'pinterest_clicks',
-            'reddit_clicks'
-        ]) }} = 0 then null
-        else (
-            {{ safe_sum([
-                'meta_mx_cost_usd',
-                'meta_non_mx_cost_usd',
-                'pinterest_cost_usd',
-                'reddit_cost_usd'
-            ]) }}
-        ) / (
-            {{ safe_sum([
-                'meta_mx_clicks',
-                'meta_non_mx_clicks',
-                'pinterest_clicks',
-                'reddit_clicks'
-            ]) }}
-        )
+    case
+        when {{ safe_sum(['meta_mx_clicks', 'meta_non_mx_clicks', 'pinterest_clicks', 'reddit_clicks']) }} = 0
+            then null
+        else
+            {{ safe_sum(['meta_mx_cost_usd', 'meta_non_mx_cost_usd', 'pinterest_cost_usd', 'reddit_cost_usd']) }}
+            / {{ safe_sum(['meta_mx_clicks', 'meta_non_mx_clicks', 'pinterest_clicks', 'reddit_clicks']) }}
     end as cpc_usd,
 
     -- CPM
-    case 
-        when {{ safe_sum([
-            'meta_mx_impressions',
-            'meta_non_mx_impressions',
-            'pinterest_impressions',
-            'reddit_impressions'
-        ]) }} = 0 then null
-        else (
-            {{ safe_sum([
-                'meta_mx_cost_usd',
-                'meta_non_mx_cost_usd',
-                'pinterest_cost_usd',
-                'reddit_cost_usd'
-            ]) }} * 1000.0
-        ) / (
-            {{ safe_sum([
-                'meta_mx_impressions',
-                'meta_non_mx_impressions',
-                'pinterest_impressions',
-                'reddit_impressions'
-            ]) }}
-        )
+    case
+        when {{ safe_sum(['meta_mx_impressions', 'meta_non_mx_impressions', 'pinterest_impressions', 'reddit_impressions']) }} = 0
+            then null
+        else
+            {{ safe_sum(['meta_mx_cost_usd', 'meta_non_mx_cost_usd', 'pinterest_cost_usd', 'reddit_cost_usd']) }} * 1000.0
+            / {{ safe_sum(['meta_mx_impressions', 'meta_non_mx_impressions', 'pinterest_impressions', 'reddit_impressions']) }}
     end as cpm_usd
 
 from {{ ref('prep_unified_social_metrics') }}
--- marketing_project/models/prep/prep_unified_social_metrics.sql
+++ marketing_project/models/prep/prep_unified_social_metrics.sql
@@ -27,11 +27,27 @@ unified as (
             coalesce(mx.date, mn.date, p.date, r.date) / 1000000000
         )::date as date,
 
-        -- Platform identifiers
-        mx.platform as meta_mx_platform,
-        mn.platform as meta_non_mx_platform,
-        p.platform as pinterest_platform,
-        r.platform as reddit_platform,
+        -- Core metadata (from your column list)
+        coalesce(mx.platform, mn.platform, p.platform, r.platform) as platform,
+        coalesce(mx.funding_source_fs, mn.funding_source_fs, p.funding_source_fs, r.funding_source_fs) as funding_source_fs,
+        coalesce(mx.kpi_pk, mn.kpi_pk, p.kpi_pk, r.kpi_pk) as kpi_pk,
+        coalesce(mx.objective_ob, mn.objective_ob, p.objective_ob, r.objective_ob) as objective_ob,
+        coalesce(mx.quarter, mn.quarter, p.quarter, r.quarter) as quarter,
+        coalesce(mx.month, mn.month, p.month, r.month) as month,
+        coalesce(mx.cw_iso, mn.cw_iso, p.cw_iso, r.cw_iso) as cw_iso,
+        coalesce(mx.year, mn.year, p.year, r.year) as year,
+        coalesce(mx.division_bs, mn.division_bs, p.division_bs, r.division_bs) as division_bs,
+        coalesce(mx.campaign_name_cn, mn.campaign_name_cn, p.campaign_name_cn, r.campaign_name_cn) as campaign_name_cn,
+
+        -- Optional high‑level attributes you mentioned (if present in staging)
+        coalesce(mx.media_channel_ch, mn.media_channel_ch, p.media_channel_ch, r.media_channel_ch) as media_channel_ch,
+        coalesce(mx.type, mn.type, p.type, r.type) as type,
+        coalesce(mx.platform_pl, mn.platform_pl, p.platform_pl, r.platform_pl) as platform_pl,
+        coalesce(mx.data_source, mn.data_source, p.data_source, r.data_source) as data_source,
+        coalesce(mx.sub_brand_sb, mn.sub_brand_sb, p.sub_brand_sb, r.sub_brand_sb) as sub_brand_sb,
+        coalesce(mx.product_category_pr, mn.product_category_pr, p.product_category_pr, r.product_category_pr) as product_category_pr,
+        coalesce(mx.mindset_md, mn.mindset_md, p.mindset_md, r.mindset_md) as mindset_md,
+        coalesce(mx.business_activity, mn.business_activity, p.business_activity, r.business_activity) as business_activity,
 
         -- Meta MX metrics
         mx.cost_usd as meta_mx_cost_usd,
@@ -59,10 +75,7 @@ unified as (
         r.impressions as reddit_impressions,
         r.clicks as reddit_clicks,
         r.total_conversions as reddit_conversions,
-        r.total_conversion_revenue_usd as reddit_revenue_usd,
-
-        -- Campaign name
-        coalesce(mx.campaign_name, mn.campaign_name, p.campaign_name, r.campaign_name) as campaign_name
+        r.total_conversion_revenue_usd as reddit_revenue_usd
 
     from meta_mx mx
     full outer join meta_non_mx mn
